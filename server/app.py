e0c02ba1f102d2861c35795c2245b601086e8d4a
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-05-04 15:07:10 +0000

    Minor changes

--- server/app.py
+++ server/app.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 import asyncio
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator, Dict
+from typing import AsyncGenerator, Dict, List
 
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import PlainTextResponse, RedirectResponse
 
 from .database import Database
 from .routes import routers
@@ -42,11 +42,33 @@ async def root() -> RedirectResponse:
 
 
 @app.get("/loop", include_in_schema=False)
-async def loop() -> str:
-    return str(asyncio.get_event_loop())
+async def loop() -> PlainTextResponse:
+    return PlainTextResponse(asyncio.get_event_loop())
 
 
 @app.get("/headers", include_in_schema=False)
 async def headers(request: Request) -> Dict[str, str]:
     """Echo client headers"""
     return dict(request.headers)
+
+
+@app.get("/whatsmyip", include_in_schema=False)
+async def whatsmyip(request: Request) -> PlainTextResponse:
+    """Echo client IP address"""
+    try:
+        return PlainTextResponse(request.headers["Client-IP"])  # For Azure web services
+
+    except KeyError:
+        addr = request.client
+        if addr is None:
+            return PlainTextResponse("Unknown", status_code=404)
+
+        return PlainTextResponse(f"{addr.host}:{addr.port}")
+
+
+@app.get(
+    "/routes",
+    summary="List all routes, including hidden ones",
+)
+async def routes(request: Request) -> List[str]:
+    return [getattr(route, "path") for route in app.routes]
